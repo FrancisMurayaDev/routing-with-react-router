c5060ecfc8b121b41b3ccd5133c9c45031e87108
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-09 12:33:54 +0300

    route and routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Fragment>
+      <h1>Website Logo</h1>
       <Routes>
         <Route path="/" element={<h1>This is the Home Page</h1>} />
         <Route path="/about" element={<h1>This is the About Page</h1>} />
