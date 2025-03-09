64280297929a1a95c184fb4adb0e10c11c15dd19
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-09 12:19:14 +0300

    wrap my route inside routes component. Each route take path and element attribute

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import "./App.css";
+import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
-
-import { Fragment } from 'react'
-
-function App () {
-  return(
+function App() {
+  return (
     <Fragment>
-      <h1>Hello</h1>
-      <p>Learning React Routing</p>
+      <Routes>
+        <Route path="/" element={<h1>This is the Home Page</h1>} />
+        <Route path="/about" element={<h1>This is the About Page</h1>} />
+        <Route path="/contact" element={<h1>This is the contacts page</h1>} />
+      </Routes>
     </Fragment>
-  )
+  );
 }
 
 export default App;
